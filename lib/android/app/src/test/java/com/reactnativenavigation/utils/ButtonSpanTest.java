3fae8ededc31e4d10e9e4153a614e9f3d159d650
Guy Carmeli <guyca@users.noreply.github.com>
2020-06-14 15:05:13 +0300

    Fix crash when disabledButtonColor is undefined and attempting to show a disabled button (#6291)
    When attempting to show a disabled TopBar button, RNN attempted to obtain the `disabledButtonColor` option even it was undefined which led to a crash.
    
    closes #6257
    closes #6204

---
+++ lib/android/app/src/test/java/com/reactnativenavigation/utils/ButtonSpanTest.java
@@ -0,0 +1,51 @@
+package com.reactnativenavigation.utils;
+
+import android.graphics.Color;
+import android.graphics.Paint;
+
+import com.reactnativenavigation.BaseTest;
+import com.reactnativenavigation.parse.params.Bool;
+import com.reactnativenavigation.parse.params.Button;
+import com.reactnativenavigation.parse.params.Colour;
+
+import org.jetbrains.annotations.NotNull;
+import org.junit.Test;
+
+import static com.reactnativenavigation.utils.ButtonSpan.DISABLED_COLOR;
+import static org.assertj.core.api.Java6Assertions.assertThat;
+
+public class ButtonSpanTest extends BaseTest {
+    private ButtonSpan uut;
+    private Button button;
+
+    @Override
+    public void beforeEach() {
+        button = createButton();
+        uut = new ButtonSpan(button);
+    }
+
+    @Test
+    public void apply_color() {
+        Paint paint = new Paint();
+        uut.apply(paint);
+
+        assertThat(paint.getColor()).isEqualTo(button.color.get());
+    }
+
+    @Test
+    public void apply_disabledColor() {
+        button.enabled = new Bool(false);
+
+        Paint paint = new Paint();
+        uut.apply(paint);
+
+        assertThat(paint.getColor()).isEqualTo(DISABLED_COLOR);
+    }
+
+    @NotNull
+    private Button createButton() {
+        Button button = new Button();
+        button.color = new Colour(Color.RED);
+        return button;
+    }
+}
