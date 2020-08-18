57519cab1cac1a4c6ebe9165db3bd4a4449d3720
Danilo Bürger <info@danilobuerger.de>
2020-08-18 15:15:27 +0200

    Enable PlatformColor / DynamicColorIOS for iOS (#6477)
    Co-authored-by: Guy Carmeli <guyca@users.noreply.github.com>

--- lib/ios/ColorParser.m
+++ lib/ios/ColorParser.m
@@ -7,7 +7,7 @@
 
 + (Color *)parse:(NSDictionary *)json key:(NSString *)key {
 	if (json[key]) {
-		return [json[key] isKindOfClass:[NSNumber class]] ? [[Color alloc] initWithValue:[RCTConvert UIColor:json[key]]] : [NoColor new];
+		return [[Color alloc] initWithValue:[RCTConvert UIColor:json[key]]] ?: [NoColor new];
 	}
 	return [NullColor new];
 }
