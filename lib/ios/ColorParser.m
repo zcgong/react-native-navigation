6022f434a8b4cd4a1b03f1f9f2a13e6912b7e07e
Yogev Ben David <yogev132@gmail.com>
2020-08-24 16:15:20 +0300

    Fix color parser (#6504)
    Return `NoColor` object when `NoColor` string is passed as the color from JS.

--- lib/ios/ColorParser.m
+++ lib/ios/ColorParser.m
@@ -6,10 +6,10 @@
 @implementation ColorParser
 
 + (Color *)parse:(NSDictionary *)json key:(NSString *)key {
-	if (json[key]) {
-		return [[Color alloc] initWithValue:[RCTConvert UIColor:json[key]]] ?: [NoColor new];
-	}
-	return [NullColor new];
+    if ([json[key] isEqual:@"NoColor"]) return [NoColor new];
+    else if (json[key]) return [Color withValue:[RCTConvert UIColor:json[key]]];
+    
+    return [NullColor new];
 }
 
 @end
