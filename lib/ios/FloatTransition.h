4a6bc8bfa5c316fb242bdab659a9154820da6535
Yogev Ben David <yogev132@gmail.com>
2020-09-16 10:41:29 +0300

    Fix shared element x y translation (#6592)
    Co-authored-by: Guy Carmeli <guyca@users.noreply.github.com>

--- lib/ios/FloatTransition.h
+++ lib/ios/FloatTransition.h
@@ -7,6 +7,10 @@
 
 - (instancetype)initWithView:(UIView *)view fromFloat:(CGFloat)from toFloat:(CGFloat)to startDelay:(NSTimeInterval)startDelay duration:(NSTimeInterval)duration interpolation:(Text *)interpolation;
 
+- (CGFloat)calculateFrom:(Double*)from;
+
+- (CGFloat)calculateTo:(Double*)to;
+
 @property (readonly) CGFloat initialValue;
 @property (nonatomic) CGFloat from;
 @property (nonatomic) CGFloat to;
--- lib/ios/HorizontalTranslationTransition.m
+++ lib/ios/HorizontalTranslationTransition.m
@@ -3,12 +3,20 @@
 @implementation HorizontalTranslationTransition
 
 - (CATransform3D)animateWithProgress:(CGFloat)p {
-    CGFloat x = [RNNInterpolator fromFloat:self.from + self.to toFloat:self.to precent:p interpolation:self.interpolation];
-    return CATransform3DMakeTranslation(x - self.to, 0, 0);
+    CGFloat x = [RNNInterpolator fromFloat:self.from toFloat:self.to precent:p interpolation:self.interpolation];
+    return CATransform3DMakeTranslation(x, 0, 0);
 }
 
 - (CGFloat)initialValue {
     return self.view.frame.origin.x;
+}
+
+- (CGFloat)calculateFrom:(Double *)from {
+    return from.hasValue ? from.get : 0;
+}
+
+- (CGFloat)calculateTo:(Double *)to {
+    return to.hasValue ? to.get : 0;
 }
 
 @end
