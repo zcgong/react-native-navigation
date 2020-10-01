6d18118f74eee6d790974694216e2f70f8103e42
Danilo Bürger <info@danilobuerger.de>
2020-10-01 15:45:30 +0200

    Added UIWindow init to playground to demonstrate usage (#6629)
    This change aligns the app delegate a bit more with the current rn hello world template.

--- playground/ios/playground/AppDelegate.m
+++ playground/ios/playground/AppDelegate.m
@@ -13,6 +13,14 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+	self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
+	if (@available(iOS 13.0, *)) {
+		self.window.backgroundColor = [UIColor systemBackgroundColor];
+	} else {
+		self.window.backgroundColor = [UIColor whiteColor];
+	}
+	[self.window makeKeyWindow];
+
 	[ReactNativeNavigation bootstrapWithDelegate:self launchOptions:launchOptions];
 	[ReactNativeNavigation registerExternalComponent:@"RNNCustomComponent" callback:^UIViewController *(NSDictionary *props, RCTBridge *bridge) {
 		return [[RNNCustomViewController alloc] initWithProps:props];
