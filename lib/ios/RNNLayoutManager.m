0ab951d1ae72a17c79969852219bd78f67584d5b
Danilo Bürger <info@danilobuerger.de>
2020-10-22 11:17:54 +0200

    Removed unneeded check that happens on recursion anyway (#6706)
    * Lowercased method name
    
    * Linting
    
    * Removed unneeded check that happens on recursion anyway

--- lib/ios/RNNLayoutManager.m
+++ lib/ios/RNNLayoutManager.m
@@ -1,4 +1,3 @@
-
 #import "RNNLayoutManager.h"
 #import "RNNLayoutProtocol.h"
 #import "UIViewController+LayoutProtocol.h"
@@ -6,8 +5,8 @@
 @implementation RNNLayoutManager
 
 + (UIViewController *)findComponentForId:(NSString *)componentId {
-	for (UIWindow* window in UIApplication.sharedApplication.windows) {
-		UIViewController* result = [self findChildComponentForParent:window.rootViewController ForId:componentId];
+	for (UIWindow *window in UIApplication.sharedApplication.windows) {
+		UIViewController *result = [self findChildComponentForParent:window.rootViewController forId:componentId];
 		if (result) {
 			return result;
 		}
@@ -16,25 +15,20 @@
 	return nil;
 }
 
-+ (UIViewController *)findChildComponentForParent:(UIViewController *)parentViewController ForId:(NSString *)componentId {
++ (UIViewController *)findChildComponentForParent:(UIViewController *)parentViewController forId:(NSString *)componentId {
 	if ([parentViewController.layoutInfo.componentId isEqualToString:componentId]) {
 		return parentViewController;
 	}
 	
 	if (parentViewController.presentedViewController) {
-		if ([parentViewController.presentedViewController.layoutInfo.componentId isEqualToString:componentId]) {
-			return parentViewController.presentedViewController;
-		}
-		
-		UIViewController* modalResult = [self findChildComponentForParent:parentViewController.presentedViewController ForId:componentId];
+		UIViewController *modalResult = [self findChildComponentForParent:parentViewController.presentedViewController forId:componentId];
 		if (modalResult) {
 			return modalResult;
 		}
-		
 	}
 	
-	for (UIViewController* childVC in parentViewController.childViewControllers) {
-		UIViewController* result = [self findChildComponentForParent:childVC ForId:componentId];
+	for (UIViewController *childVC in parentViewController.childViewControllers) {
+		UIViewController *result = [self findChildComponentForParent:childVC forId:componentId];
 		if (result) {
 			return result;
 		}
@@ -42,6 +36,5 @@
 	
 	return nil;
 }
-
 
 @end
