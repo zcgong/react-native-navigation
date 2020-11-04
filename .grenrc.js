553073c85a17c62e41c5fde0acb8645b05695d05
Yogev Ben David <yogev132@gmail.com>
2020-11-04 22:40:32 +0200

    Group issues using groupPostProcessor (#6741)

--- .grenrc.js
+++ .grenrc.js
@@ -2,10 +2,10 @@ module.exports = {
   template: {
     commit: ({message, url, author, name}) =>
       `- [${message}](${url}) - ${author ? `@${author}` : name}`,
-    issue: '- {{name}} [{{text}}]({{url}}) by [{{user_login}}](https://github.com/{{user_login}})",',
+    issue: ({name, labels, text, url, user_login, user_url}) => `${processLabels(labels)}${name} [${text}](${url}) by [${user_login}](${user_url})`,
     label: '[**{{label}}**]',
     noLabel: 'closed',
-    group: '\n#### {{heading}}\n',
+    group: '\n## {{heading}}\n',
     changelogTitle: '# Changelog\n\n',
     release: '## {{release}} ({{date}})\n{{body}}',
     releaseSeparator: '\n---\n\n',
@@ -15,6 +15,23 @@ module.exports = {
     'Bug fixes:': ['type: accepted/bug'],
     Features: ['feature'],
   },
+  groupPostProcessor: (groupContent) => {
+    const lines = groupContent.split('\n');
+    const iosIssues = [];
+    const androidIssues = [];
+    const otherIssues = [];
+
+    for (let i = 0; i < lines.length; i++) {
+      const line = lines[i];
+      if (line.includes('## ')) continue;
+      else if (line.includes('[iOS] ')) iosIssues.push(line.replace('[iOS] ', ''));
+      else if (line.includes('[Android] ')) androidIssues.push(line.replace('[Android] ', ''));
+      else otherIssues.push(line);
+    }
+
+    const groupHeader = groupContent.substr(0, groupContent.indexOf(':\n'));
+    return `${groupHeader}${generateSection(undefined, otherIssues)}${generateSection('iOS', iosIssues)}${generateSection('Android', androidIssues)}`;
+  },
   ignoreIssuesWith: ['skip-changelog'],
   ignoreTagsWith: ['snapshot', 'v1', 'v2', '0\..\..', '1\..\..', '2\..\..', '3\..\..', '4\..\..', '5\..\..', '6\..\..'],
   dataSource: 'prs',
@@ -23,3 +40,24 @@ module.exports = {
   generate: true,
   tags: 'all'
 };
+
+function generateSection(name, issues) {
+  if (!issues.length) return '';
+  let section = `${name ? `### ${name}\n` : ''}`;
+
+  issues.forEach(issue => {
+    section += `${issue}\n`;
+  });
+
+  return `${section}\n`;
+}
+
+function processLabels(labels) {
+  if (labels.includes('**platform: iOS**')) {
+    return '[iOS] '
+  } else if (labels.includes('**platform: Android**')) {
+    return '[Android] '
+  }
+
+  return '';
+}
